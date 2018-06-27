4e5af7a5422df5bc508923c736ab2b53ab06b99f
hunkr11 <hunkr11@gmail.com>
2018-06-27 09:59:23 +0530

    for heroku deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FileUploadComponent } from './pages/licence-class-master/file-upload';
 import { FormsModule } from '@angular/forms';
 import { LicenceService } from './service/licence.service';
 import {HttpClientModule} from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,HttpModule,FormsModule,HttpClientModule
   ],
-  providers: [LicenceService],
+  providers: [LicenceService,{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
